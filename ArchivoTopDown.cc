6a583eddd2bba994ff750cc064913ca7aca3b587
martin <martinnufa@gmail.com>
2025-06-27 21:24:56 -0300

    ahora si lee bien el archivo y carga las líneas en el vector, soy muy bueno

--- ArchivoTopDown.cc
+++ ArchivoTopDown.cc
@@ -1,19 +1,14 @@
 #include <iostream>
-#include <iterator>
 #include <string>
 #include <fstream>
 #include <vector>
-#include "LineaTopDown.h"
 #include "ErrorHandler.h"
 #include "ArchivoTopDown.h"
 ArchivoTopDown::ArchivoTopDown(){
 	LineasArchivo.reserve(10);
-	std::cout << "ahora el vector cuenta con " << LineasArchivo.capacity() << "lineas guardadas" << std::endl;
-	std::cout << "memoria asignada exitosamente" << std::endl;
 }
 ArchivoTopDown::~ArchivoTopDown(){
 	LineasArchivo.clear();
-	std::cout << "memoria liberada exitosamente" << std::endl;
 }
 void ArchivoTopDown::leeDesdeArchivo(){
 	int cantidadLineas = 0;
@@ -23,25 +18,22 @@ void ArchivoTopDown::leeDesdeArchivo(){
 		if(!entrada){
 			throw(ErrorHandler(TipoError::ERROR_LINEA_ABRIR_ARCHIVO));
 		}
+		entrada.seekg(0,entrada.beg);
 		std::getline(entrada,auxiliar);
 		while(!entrada.eof()){
 			cantidadLineas ++;
 			std::getline(entrada,auxiliar);
 		}
-		entrada.close();
+		entrada.clear();
 		if(cantidadLineas==0){
 			throw(ErrorHandler(TipoError::ERROR_LINEA_ARCHIVO_VACÍO));
 		}
 		LineasArchivo.resize(cantidadLineas);
-		std::cout << "ahora el vector cuenta con " << LineasArchivo.capacity() << "lineas guardadas" << std::endl;
-		// si todo esto salió bien, se leerá todo en serio
-		// apuntamos al principio del archivo y leemos
-		entrada.seekg(0);
-		std::getline(entrada,auxiliar);
-		for(int i = 0; i < LineasArchivo.capacity() ;i++){
-			LineasArchivo.push_front(auxiliar);
-			std::getline(entrada,auxiliar);
+		entrada.seekg(0,entrada.beg);
+		for(int i = 0; i < cantidadLineas ;i++){
+			LineasArchivo.emplace_back(auxiliar);
 		}
+		entrada.close();
 	}
 	catch(const ErrorHandler& error){
 		entrada.close();
